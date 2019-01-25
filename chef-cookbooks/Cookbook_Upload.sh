f876ede5bd0d7d5bdc42ffa46c29608e9d982812
shamit619 <shamit619@gmail.com>
2019-01-25 09:31:30 +0530

    Update Cookbook_Upload.sh

--- chef-cookbooks/Cookbook_Upload.sh
+++ chef-cookbooks/Cookbook_Upload.sh
@@ -3,15 +3,18 @@
 cookbook_name=$1
 cd /mnt/recovery/root/chef-cookbooks
 rm -rf IAC_Cookbooks
-git clone https://github.com/shamit619/IAC_Cookbooks.git
+#git clone https://github.com/shamit619/IAC_Cookbooks.git
 if [[ "$cookbook_name" == "jenkins-demo" ]];
 then
 echo "hi"
-cd /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo/cookbooks/jenkins-demo
+#cd /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo/cookbooks/jenkins-demo
+cd /home/ec2-user/Scenario-303/cookbooks/jenkins-demo
 berks install
 berks upload
-knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo/cookbooks -a
+knife cookbook upload --force --cookbook-path /home/ec2-user/Scenario-303/cookbooks -a
+#knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo/cookbooks -a
+else
+knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/$cookbook_name/cookbooks -a
 fi
 
-knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/$cookbook_name/cookbooks -a
 
