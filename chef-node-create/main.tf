c99cef647a5fd9683a6b0cfd59909fc61bea7bf2
shamit619 <shamit619@gmail.com>
2019-01-27 09:59:06 +0530

    Update main.tf

--- chef-node-create/main.tf
+++ chef-node-create/main.tf
@@ -40,10 +40,8 @@ resource "aws_instance" "chef" {
 }
 
 resource "aws_security_group" "instance" {
+  count = "${var.node-reattempt ? 1 : 0}"
   name = "chef-security-node"
-  lifecycle {
-    create_before_destroy = true
-  }
 
   # Inbound HTTP from anywhere
   ingress {
