ee4ead1e3c9ae64cb53a23798a11ec1171dbbf6d
shamit619 <shamit619@gmail.com>
2019-01-29 09:10:06 +0530

    Update Cookbook_Upload.sh

--- chef-cookbooks/Cookbook_Upload.sh
+++ chef-cookbooks/Cookbook_Upload.sh
@@ -7,10 +7,10 @@ git clone https://github.com/shamit619/IAC_Cookbooks.git
 if [[ "$cookbook_name" == "jenkins-demo" ]];
 then
 echo "hi"
-cd /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo-updated/cookbooks/jenkins-demo
+cd /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo-latest/cookbooks/jenkins-demo
 berks install
 berks upload
-knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo-updated/cookbooks -a
+knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/jenkins-demo-latest/cookbooks -a
 else
 knife cookbook upload --force --cookbook-path /mnt/recovery/root/chef-cookbooks/IAC_Cookbooks/$cookbook_name/cookbooks -a
 fi
