169d08efec83667ab537441f081e5986eec6d62d
shamit619 <shamit619@gmail.com>
2019-02-19 19:15:16 +0530

    Update add_role_run_list.sh

--- chef-add-run-list/add_role_run_list.sh
+++ chef-add-run-list/add_role_run_list.sh
@@ -3,15 +3,15 @@ recipe_name=$1
 server_name=$2
 #recipe_name2=$3
 
-knife role from file /mnt/recovery/root/chef-add-run-list/roles/chef-client-interval.json
+#knife role from file /mnt/recovery/root/chef-add-run-list/roles/chef-client-interval.json
 
 knife node list > /mnt/recovery/root/chef-add-run-list/node_name.txt
 
-while read node
-do
-echo "attaching role"
-knife node run_list set $node "role[chef-client-interval]"
-done < /mnt/recovery/root/chef-add-run-list/node_name.txt
+#while read node
+#do
+#echo "attaching role"
+#knife node run_list set $node "role[chef-client-interval]"
+#done < /mnt/recovery/root/chef-add-run-list/node_name.txt
 
 knife node list | grep "$server_name" > /mnt/recovery/root/chef-add-run-list/server_name.txt
 
@@ -23,15 +23,16 @@ done < /mnt/recovery/root/chef-add-run-list/server_name.txt
 
 #rm -rf /mnt/recovery/root/chef-add-run-list/node_name.txt /mnt/recovery/root/chef-add-run-list/server_name.txt
 
-cd /mnt/recovery/root/chef-node-create
+#cd /mnt/recovery/root/chef-node-create
 
-terraform output chef-node-public-IP | awk -F',' '{print $1}' > /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
-cat /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
-while read  public_ip
+#terraform output chef-node-public-IP | awk -F',' '{print $1}' > /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
+#cat /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
+while read  node
 do
-echo $public_ip
-knife ssh 'role:chef-client-interval' 'sudo chef-client' -x ubuntu -i ~/.ssh/terraform-cheff.pem -a $public_ip
-done < /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
+echo $node
+#knife ssh 'role:chef-client-interval' 'sudo chef-client' -x ubuntu -i ~/.ssh/terraform-cheff.pem -a $public_ip
+knife ssh name:$node 'sudo chef-client' -x ubuntu -i ~/.ssh/chef-workstation.pem
+done < /mnt/recovery/root/chef-add-run-list/node_name.txt
 
 #rm -rf /mnt/recovery/root/chef-add-run-list/node-public-ip.txt
 
